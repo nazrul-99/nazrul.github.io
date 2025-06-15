711185a42c837c33d35db5e28bc596ba3461ffed
nazrul-99 <nazrul.kuet17@gmail.com>
2025-06-15 21:32:19 +0600

    Implemented dark mode using useeffect() method which need use client to be mentioned

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
+"use client"; 
 import Image from "next/image";
 
+import { useEffect } from "react";
+
+
 export default function Home() {
+  useEffect(() => {
+    // Enable dark mode globally
+    document.documentElement.classList.add("dark");
+  }, []);
   return (
     <main>Homepage</main>
   );
--- components/Header.jsx
+++ components/Header.jsx
@@ -2,24 +2,33 @@ import Link from "next/link";
 import {Button} from "./ui/button";
 
 
+
 //components
 import Nav from "./Nav";
 
 const Header = () => {
     return(
-        <header className="dark">
+        <header className="py-8 xl:py-12 text-foreground">
             <div className="container mx-auto"> 
+
                 {/*logo*/}
                 <Link href="/">
                     <h1 className="text-4xl font-semibold">
                         Md Nazrul Islam<span className="text-accent">.</span> 
                     </h1>
                 </Link>
+
                 {/* desktop nav */}
-                <Nav/>
+                <div className="hidden xl:flex">
+                    <Nav/>
+                    <Link href="/contact">
+                    <Button>Contact Me</Button>
+                    </Link>
+                </div>
+                
             </div>
         </header>
     )
 }
 
-export default Header
+export default Header;
