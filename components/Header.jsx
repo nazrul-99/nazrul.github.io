68459e31e325ea12e4d3b4a5c96672c6ac4b1216
nazrul-99 <nazrul.kuet17@gmail.com>
2025-06-15 20:13:55 +0600

    Adding nav bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,24 @@
+import Link from "next/link";
+import {Button} from "./ui/button";
+
+
+//components
+import Nav from "./Nav";
+
 const Header = () => {
     return(
-        <header>header</header>
+        <header className="dark">
+            <div className="container mx-auto"> 
+                {/*logo*/}
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Md Nazrul Islam<span className="text-accent">.</span> 
+                    </h1>
+                </Link>
+                {/* desktop nav */}
+                <Nav/>
+            </div>
+        </header>
     )
 }
 
