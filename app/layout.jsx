6471e29cdc17fb7fecbdbe1ee8654f8b654fffe8
nazrul-99 <nazrul.kuet17@gmail.com>
2025-06-15 15:31:00 +0600

    In components Header.jsx file is added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+// Components
+import Header from "@/components/Header"
+
 const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"], 
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"], 
   variable: '--font-jetBrainsMono'
@@ -15,6 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetBrainsMono.variable}>
+        <Header/>
         {children}
       </body>
     </html>
