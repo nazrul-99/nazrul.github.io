7f9f4d44fc8ba41ab3db275a570128df2c31cc88
nazrul-99 <nazrul.kuet17@gmail.com>
2025-06-19 23:48:29 +0600

    Homepage making in progress, added social icons and photo

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,54 @@
-// "use client"; 
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import{ FiDownload } from "react-icons/fi"
 
-// import { useEffect } from "react";
+// components
+import Socials from "@/components/Socials";
+import Photo from "@/components/Photo";
 
 
-export default function Home() {
-  // useEffect(() => {
-  //   // Enable dark mode globally
-  //   document.documentElement.classList.add("dark");
-  // }, []);
+const Home = () => {
   return (
-    <main>homepage</main>
-  );
-}
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between
+        xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Lecturer and PhD Aspirant</span>
+            <h1 className="h1">
+              Hello I'm <br /> <span className="text-accent">Md Nazrul Islam</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+             I have researched various topics for 
+              three years to find my passion: machine learning, 
+              deep learning, LLMs, and signals for medical and 
+              healthcare applications.
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-6">
+              <Button
+                variant="outline" 
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent
+                rounded-full flex justify-center items-center text-accent text-base hover:bg-accent
+                hover:text-primary hover:transition-all duration-500"/>
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>
+            <Photo />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default Home;
---
+++ components/Photo.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const Photo = () => {
+    return (
+        <div className="w-full h-full relative">
+            <motion.div>
+                <div className="w-[298px] h-[298px] xl:w-[498px] h-[498px]
+                mix-blend-lighten">
+                    <Image src="/assets/My_Photo.png" priority quality={100} fill alt=""
+                    className="object-content"/>
+                </div>
+            </motion.div>
+        </div>
+    )
+};
+
+export default Photo;
