d51ee755026d0ad9cb780912c722f25b7b2cbd33
nazrul-99 <nazrul.kuet17@gmail.com>
2025-06-21 15:21:18 +0600

    Adding number based stats

---
+++ components/Stats.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import CountUp from "react-countup";
+
+const stats = [
+    {
+        num: 1,
+        text: "Coference",
+    },
+    {
+        num: 1,
+        text: "Journals",
+    },
+    {
+        num: 1,
+        text: "Years of experience",
+    },
+    {
+        num: 1,
+        text: "Years of experience",
+    },
+];
+
+const Stats = () => {
+    return (
+        <div>Stats </div>
+    );
+};
+
+export default Stats;
