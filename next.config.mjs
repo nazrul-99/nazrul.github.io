099efb6a5dfdc5e8e506994d0924dd361fb2adb7
nazrul-99 <nazrul.kuet17@gmail.com>
2025-06-24 23:14:33 +0600

    configuring for github deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
+// add for github delpoyment
+const isGhPages = process.env.DEPLOY_TARGET === 'gh-pages';
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'portfolio';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    //add for github deployment
+    reactStrictMode: true,
+    crossOrigin: 'anonymous',
+    trailingSlash: true,
+    env: {
+        baseUrl,
+    },
+    typescript: {
+        ignoreBuildErrors: false,
+    },
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: '*',
+        },
+        ],
+    },
+    // from here Importatnt !!**
+    output: 'export',
+    basePath: isGhPages && isProd ? `/${repo}` : '',
+    assetPrefix: isGhPages && isProd ? `/${repo}/` : '',
+};
 
 export default nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+content: [
+  './pages/**/*.{js,ts,jsx,tsx,mdx}', // if using pages directory
+  './components/**/*.{js,ts,jsx,tsx,mdx}', // all components directory
+  './app/**/*.{js,ts,jsx,tsx,mdx}', // if using app directory
+  './styles/**/*.{css,scss}',
+];
