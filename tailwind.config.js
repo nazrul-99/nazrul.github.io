54e9b5ed41db81d18e7667fba7f71721066bf68e
nazrul-99 <nazrul.kuet17@gmail.com>
2025-06-25 21:36:29 +0600

    Trying to fix Github deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-content: [
+module.exports = {
+  content: [
   './pages/**/*.{js,ts,jsx,tsx,mdx}', // if using pages directory
   './components/**/*.{js,ts,jsx,tsx,mdx}', // all components directory
   './app/**/*.{js,ts,jsx,tsx,mdx}', // if using app directory
   './styles/**/*.{css,scss}',
-];
+  ],
+}
